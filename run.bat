188389ed4c1a9f5a04dae427efa0001f70c858fc
NSXC <127046038+NSXC@users.noreply.github.com>
2023-06-16 12:34:29 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 start cmd.exe /k ngrok http 80
 start cmd.exe /k grab
 start cmd.exe /k C:/Python311/python.exe "PATHTO SERVER"
-start cmd.exe /k C:/Python311/python.exe PATHTOMAIN"
+start cmd.exe /k C:/Python311/python.exe "PATHTOMAIN"
 cd "CDTORUSTPATH"
 cargo build 
 cargo run 
