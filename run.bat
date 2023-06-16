7aec787b72039d3138298366ee7cd2ad78aa2079
NSXC <127046038+NSXC@users.noreply.github.com>
2023-06-16 12:34:18 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 start cmd.exe /k ngrok http 80
 start cmd.exe /k grab
-start cmd.exe /k C:/Python311/python.exe "c:/Users/Josep/OneDrive/Desktop/Pilot OTP/server.py"
-start cmd.exe /k C:/Python311/python.exe "c:/Users/Josep/OneDrive/Desktop/Pilot OTP/main.py"
-cd "c:/Users/Josep/OneDrive/Desktop/Pilot OTP/login"
+start cmd.exe /k C:/Python311/python.exe "PATHTO SERVER"
+start cmd.exe /k C:/Python311/python.exe PATHTOMAIN"
+cd "CDTORUSTPATH"
 cargo build 
 cargo run 
